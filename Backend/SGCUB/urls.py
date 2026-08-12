16f47637d01c856264bae100786dc5d9a888e7d9
Maximo Preneste <maxipreneste@gmail.com>
2026-08-12 13:12:22 -0300

    feat(padron/views): crear los crud de entidades

--- Backend/SGCUB/urls.py
+++ Backend/SGCUB/urls.py
@@ -21,5 +21,5 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
-
+    path("api/padron/", include("padron.urls")),
 ]
