8841426ace381cc7ee598d364953c2f07001a52d
benjaelizalde <belizalde@alu.frlp.utn.edu.ar>
2026-08-27 00:59:03 -0300

    feat: agrega modelo ContactoEmergencia y actualiza campos en Persona y Jugador

--- Backend/padron/admin.py
+++ Backend/padron/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import EstadoDeportivo, Persona, Socio, Categoria, Jugador
+from .models import EstadoDeportivo, Persona, Socio, Categoria, Jugador, Docente, ContactoEmergencia
 admin.site.register(Persona)
 admin.site.register(Socio)
 admin.site.register(Categoria)
 admin.site.register(Jugador)
 admin.site.register(EstadoDeportivo)
+admin.site.register(Docente)
+admin.site.register(ContactoEmergencia)
