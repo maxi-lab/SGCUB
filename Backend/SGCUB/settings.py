9b20668203d4b0482bd8f14a6e0ddc10101dcdd1
Maximo Preneste <maxipreneste@gmail.com>
2026-09-11 00:53:54 -0300

    prueba 1 de delete branch

--- Backend/SGCUB/settings.py
+++ Backend/SGCUB/settings.py
@@ -174,3 +174,4 @@ SPECTACULAR_SETTINGS = {
     'COMPONENT_SPLIT_REQUEST': True, # Separar esquemas de Request y Response para mayor claridad3
     "SORT_OPERATIONS": True,  # ordena alfabéticamente los tags/operaciones
 }
+#prueba de cambio
