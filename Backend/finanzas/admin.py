5d0c20b67a68be26e34144d1a30214195c846bf9
Maximo Preneste <maxipreneste@gmail.com>
2026-09-11 16:56:30 -0300

    feat(Finanzas/admin):poner en el panel da administrador

--- Backend/finanzas/admin.py
+++ Backend/finanzas/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+	Comprobante,
+	CuentaCorriente,
+	Cuota,
+	CuotaXPago,
+	DetalleMedioPago,
+	EstadoCuota,
+	ItemCuota,
+	Pago,
+)
+
+admin.site.register(EstadoCuota)
+admin.site.register(Cuota)
+admin.site.register(ItemCuota)
+admin.site.register(Pago)
+admin.site.register(DetalleMedioPago)
+admin.site.register(Comprobante)
+admin.site.register(CuotaXPago)
+admin.site.register(CuentaCorriente)
