f3be5e26336b59d272ea8b0a7fe69e4f3399a993
Rodrigo Capre <rodrigocapre@gmail.com>
2026-09-12 15:02:42 -0300

    agrega descripcion en common layout

--- Frontend/SGCUB/src/layouts/CommonLayout.jsx
+++ Frontend/SGCUB/src/layouts/CommonLayout.jsx
@@ -1,12 +1,10 @@
 import { Outlet, useLocation } from 'react-router-dom'
 import './commonLayout.css'
 
-export default function CommonLayout({ moduleName, defaultDescription, getSectionName }) {
 export default function CommonLayout({ moduleName, defaultDescription, getSectionName, getSectionDescription }) {
   const { pathname } = useLocation()
   
   const sectionName = getSectionName ? getSectionName(pathname) : ''
-  const description = sectionName ? `Administración y control de ${sectionName.toLowerCase()}` : defaultDescription
   const description = getSectionDescription 
     ? getSectionDescription(pathname) 
     : (sectionName ? `Administración y control de ${sectionName.toLowerCase()}` : defaultDescription)
